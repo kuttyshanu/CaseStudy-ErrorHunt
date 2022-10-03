d7abadaba0838bbf1718d4b1ad01a3f68ae18ce0
shanu <shanuvs127@gmail.com>
2022-10-03 13:12:53 +0530

    12

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const path = require ('path'); 
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 //Part #2 Point 6
 // const nav= [                   
 //     {
